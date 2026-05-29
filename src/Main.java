289fef8c25bea812422be879a633e5d399e7c41b
3.14a <929tianye@gmail.com>
2026-05-30 01:44:20 +0800

    # 001 - basic GUI

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import GUI.*;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author tianye
+ */
+public class Main {
+    public static void main(String[] args) {
+        new LoginPage().setVisible(true);
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author tianye
+ */
+public class User {
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
